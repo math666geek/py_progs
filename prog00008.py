e7a4e432ba71c975931c1d5c80a1aa8378a28723
math666geek <117521996+math666geek@users.noreply.github.com>
2022-11-05 16:24:42 +0300

    formatting

--- prog00008.py
+++ prog00008.py
@@ -1,17 +1,18 @@
-def get_index(l,p):
-    first_index=0
-    last_index=len(l)-1
-    mid=0
-    while first_index<=last_index:
-        mid=(first_index+last_index)//2
-        if l[mid]==p:
+def get_index(l, p):
+    first_index = 0
+    last_index = len(l)-1
+    mid = 0
+    while first_index <= last_index:
+        mid = (first_index+last_index) // 2
+        if l[mid] == p:
             return mid
-        elif p<l[mid]:
-            last_index=mid-1
+        elif p < l[mid]:
+            last_index = mid - 1
         else:
-            first_index=mid+1
+            first_index = mid + 1
     return mid
-        
-l=[2,5,7,18,20]
-p=20
-print(get_index(l,p))
+
+
+l = [2, 5, 7, 18, 20]
+p = 20
+print(get_index(l, p))
