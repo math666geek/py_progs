4e90af097812e3c98123659548c61135beb58c68
math666geek <117521996+math666geek@users.noreply.github.com>
2022-11-27 19:20:59 +0300

    27.11.2022

--- cryptographer_2.py
+++ cryptographer_2.py
@@ -1,16 +1,19 @@
-text=''
-with open('output_text_cipher.txt', 'r', encoding="utf-8") as file:
+text = ''
+with open('text_for_prog.txt', 'r', encoding="utf-8") as file:
     for line in file:
         text = text + line
+exceptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',', '.', '_', '-', '=', '+', '?', '!', '&', '\n', ':', '\xa0', '«', '»', '–', ';']
 text = text.lower()
-s_2={}
+for item in exceptions:
+    text = text.replace(item, '')
+s_2 = {}
 for i in text:
-    c=0
-    f=0
+    c = 0
+    f = 0
     if i not in s_2:
         c = text.count(i)
-        f=c/len(text)
-        s_2[i]=f
+        f = c / len(text)
+        s_2[i] = f
     else:
         continue
 print(s_2)
